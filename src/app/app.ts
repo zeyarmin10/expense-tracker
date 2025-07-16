833620960b18dab6497e1c17aabfdea21715d31e
Zeyar <zeyarmin@brycencambodia.com.kh>
2025-07-16 13:37:04 +0700

    fix expense minor bugs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faEdit, faTrashAlt, faPlusCircle, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('expense-tracker');
+  constructor(library: FaIconLibrary) {
+    // သင်သုံးမယ့် icon တွေကို library ထဲ ထည့်ပါ
+    library.addIcons(faEdit, faTrashAlt, faPlusCircle, faSave, faTimes);
+  }
 }
