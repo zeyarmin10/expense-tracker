75bce755bc4f601393ce1505f35222b5c2bcf868
Zeyar <93753945+zeyarmin10@users.noreply.github.com>
2025-07-17 13:59:09 +0700

    add login background

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { Expense } from './components/expense/expense';
-// import { Profit } from './components/profit/profit';
-// import { Category } from './components/category/category';
-// import { LoginComponent } from './components/login/login';
-// import { authGuard } from './guards/auth.guard';
-// import { publicGuard } from './guards/public.guard';
-
-// export const routes: Routes = [
-//   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route redirects to login
-//   { path: '**', redirectTo: '/login' }, // Wildcard route redirects to login
-//   { path: 'login', component: LoginComponent, canActivate: [publicGuard] },
-//   { path: 'expense', component: Expense, canActivate: [authGuard] },
-//   { path: 'profit', component: Profit, canActivate: [authGuard] },
-//   { path: 'category', component: Category, canActivate: [authGuard] }
-// ];
-
-
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { Expense } from './components/expense/expense';
