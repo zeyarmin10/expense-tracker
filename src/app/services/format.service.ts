85182616b12c3f40c971ee5c95fc0a239e30755d
Zeyar <93753945+zeyarmin10@users.noreply.github.com>
2025-09-18 07:38:58 +0700

    update currency for Burmese

--- src/app/services/format.service.ts
+++ src/app/services/format.service.ts
@@ -57,11 +57,10 @@ export class FormatService {
     //   return `${symbol}${formattedAmount}`;
     // }
 
-    if (locale === BURMESE_LOCALE_CODE) {
-      return `${formattedAmount}${symbol}`;
-    }
     if (locale === BURMESE_LOCALE_CODE && currency === MMK_CURRENCY_CODE) {
       return `${formattedAmount} ${BURMESE_CURRENCY_SYMBOL}`;
+    } else if (locale === BURMESE_LOCALE_CODE) {
+      return `${formattedAmount}${symbol}`;
     }
 
     return `${symbol}${formattedAmount}`;
